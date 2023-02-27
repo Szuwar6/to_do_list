72b712e6464bd14d3dac8c8e3a31b36c8e0e5e38
Marek <marekszubartowicz@gmail.com>
2023-02-27 21:03:16 +0100

    commit

--- src/services/create.py
+++ src/services/create.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
 from flask import request
-
+from src.models.tasks import Tasks
 from src.serializers.tasks import task_schema
 from src.models.helpers import save_to_db
 
@@ -12,8 +12,11 @@ class CreateTaskView(MethodView):
 
     def post(self):
         body = request.get_json()
-        task = task_schema.load(body)
+        id = body.get('id')
+        if Tasks.query.filter_by(id=id) is not None:
+            return "Task already exists", 409
 
+        task = task_schema.load(body)
         save_to_db(task)
 
         return "Task's been created successfully!", 201
